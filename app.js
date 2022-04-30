5b1d534445b12a0a05d76fb863a16c2044a49a20
Dalveer Singh <57098009+dalveer16@users.noreply.github.com>
2022-04-30 15:33:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function check() {
+  var y = document.getElementById("project");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
